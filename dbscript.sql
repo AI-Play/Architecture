704ebae5569b921610d572d3f4e74c5eed1d5f7b
seahahn <seah.ahn.nt@gmail.com>
2022-03-05 17:27:07 +0900

    dbscript updated

--- dbscript.sql
+++ dbscript.sql
@@ -1,14 +1,19 @@
--- Drop table
+
+-- users definition
+
+-- user 키워드는 예약어이므로 테이블명으로 사용하지 않음
+-- ('user'로 따옴표 붙이면 불가능은 아니나, 이후 테이블명 사용 시 무조건 따옴표 붙여야 해서 불편함)
+-- 참고 링크 : https://stackoverflow.com/questions/22256124/cannot-create-a-database-table-named-user-in-postgresql
 
 -- DROP TABLE users;
 
-CREATE TABLE users (
-	idx serial NOT NULL, -- 유저 구분
-	email varchar(255) NOT NULL,
-	pw varchar(255) NOT NULL, -- 패스워드
-	nickname varchar(255) NOT NULL,
-	membership int NOT NULL, -- 멤버쉽 구분(티어1, 티어2 등)
-	last_in TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 마지막 접속 시점
+CREATE TABLE users(
+	idx SERIAL NOT NULL, -- 사용자 고유 번호
+	email VARCHAR(255) NOT NULL,
+	pw VARCHAR(255) NOT NULL, -- 패스워드
+	nickname VARCHAR(255) NOT NULL,
+	membership INTEGER NOT NULL, -- 멤버쉽 구분(티어1, 티어2 등)
+	last_update TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 마지막 접속 시점
 	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 가입 시점
 	CONSTRAINT users_pk PRIMARY KEY (idx)
 );
@@ -16,47 +21,41 @@ COMMENT ON TABLE users IS '사용자 정보';
 
 -- Column comments
 
-COMMENT ON COLUMN users.idx IS '유저 구분';
+COMMENT ON COLUMN users.idx IS '사용자 고유 번호';
 COMMENT ON COLUMN users.pw IS '패스워드';
 COMMENT ON COLUMN users.membership IS '멤버쉽 구분(티어1, 티어2 등)';
-COMMENT ON COLUMN users.last_in IS '마지막 접속 시점';
+COMMENT ON COLUMN users.last_update IS '마지막 접속 시점';
 COMMENT ON COLUMN users.created_at IS '가입 시점';
 
 
 
+-- func_log definition
 
+-- DROP TABLE func_log;
 
--- service_times definition
-
--- Drop table
-
--- DROP TABLE service_times;
-
-CREATE TABLE service_times (
-	user_idx int4 NOT NULL, -- 사용자 고유번호
-	func_code varchar(255) NOT NULL, -- 함수 기능 코드
+CREATE TABLE func_log (
+	user_idx INTEGER NOT NULL, -- 사용자 고유번호
+	func_code VARCHAR(255) NOT NULL, -- 함수 기능 코드
 	is_worked bool NOT NULL, -- 정상 작동 여부
 	start_time TIMESTAMP NOT NULL, -- 작동 시작 시각
 	end_time TIMESTAMP NOT NULL, -- 작동 종료 시각
 	created_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW() -- 생성 시점
 );
-COMMENT ON TABLE service_times IS '사용자 서비스 이용시간 + 기능별 사용 횟수 체크';
+COMMENT ON TABLE func_log IS '사용자 서비스 이용시간 + 기능별 사용 횟수 체크';
 
 -- Column comments
 
-COMMENT ON COLUMN service_times.user_idx IS '사용자 고유번호';
-COMMENT ON COLUMN service_times.func_code IS '함수 기능 코드';
-COMMENT ON COLUMN service_times.is_worked IS '정상 작동 여부';
-COMMENT ON COLUMN service_times.start_time IS '작동 시작 시각';
-COMMENT ON COLUMN service_times.end_time IS '작동 종료 시각';
-COMMENT ON COLUMN service_times.created_time IS '생성 시점';
+COMMENT ON COLUMN func_log.user_idx IS '사용자 고유번호';
+COMMENT ON COLUMN func_log.func_code IS '함수 기능 코드';
+COMMENT ON COLUMN func_log.is_worked IS '정상 작동 여부';
+COMMENT ON COLUMN func_log.start_time IS '작동 시작 시각';
+COMMENT ON COLUMN func_log.end_time IS '작동 종료 시각';
+COMMENT ON COLUMN func_log.created_time IS '생성 시점';
 
 
--- service_times foreign keys
+-- func_log foreign keys
 
-ALTER TABLE service_times ADD CONSTRAINT service_times_fk FOREIGN KEY (user_idx) REFERENCES users(idx);
-
-
+ALTER TABLE func_log ADD CONSTRAINT func_log_fk FOREIGN KEY (user_idx) REFERENCES users(idx);
 
 
 
@@ -68,8 +67,8 @@ ALTER TABLE service_times ADD CONSTRAINT service_times_fk FOREIGN KEY (user_idx)
 
 CREATE TABLE ml_project (
 	idx serial NOT NULL, -- 프로젝트 고유 번호
-	user_idx int NOT NULL, -- 사용자 고유 번호
-	proj_name varchar(255) NOT NULL, -- 프로젝트 명
+	user_idx INTEGER NOT NULL, -- 사용자 고유 번호
+	proj_name VARCHAR(255) NOT NULL, -- 프로젝트 명
 	last_update TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 마지막 수정 시점
 	created_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 생성 시점
 	CONSTRAINT ml_project_pk PRIMARY KEY (idx)
@@ -99,8 +98,8 @@ ALTER TABLE ml_project ADD CONSTRAINT ml_project_fk FOREIGN KEY (user_idx) REFER
 
 CREATE TABLE ml_model (
 	idx serial NOT NULL, -- 모델 고유 번호
-	user_idx int NOT NULL, -- 사용자 고유 번호
-	model_name varchar(255) NOT NULL, -- 모델명
+	user_idx INTEGER NOT NULL, -- 사용자 고유 번호
+	model_name VARCHAR(255) NOT NULL, -- 모델명
 	last_update TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 마지막 수정 시점
 	created_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 생성 시점
 	CONSTRAINT ml_model_pk PRIMARY KEY (idx)
