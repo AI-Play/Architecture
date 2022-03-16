7650d9e41837599e410f5d032ae596d2960cef9b
seahahn <seah.ahn.nt@gmail.com>
2022-03-17 03:16:08 +0900

    dbscript updated

--- dbscript.sql
+++ dbscript.sql
@@ -42,7 +42,7 @@ CREATE TABLE func_log (
 	error_msg TEXT NOT NULL DEFAULT '', -- Unexpected Error일 경우 저장되는 에러 메시지
 	start_time TIMESTAMP NOT NULL, -- 작동 시작 시각
 	end_time TIMESTAMP NOT NULL, -- 작동 종료 시각
-	created_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW() -- 생성 시점
+	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW() -- 생성 시점
 );
 COMMENT ON TABLE func_log IS '사용자 서비스 이용시간 + 기능별 사용 횟수 체크';
 
@@ -54,7 +54,7 @@ COMMENT ON COLUMN func_log.is_worked IS '정상 작동 여부';
 COMMENT ON COLUMN func_log.error_msg IS 'Unexpected Error(is_worked=2)일 경우 저장되는 에러 메시지';
 COMMENT ON COLUMN func_log.start_time IS '작동 시작 시각';
 COMMENT ON COLUMN func_log.end_time IS '작동 종료 시각';
-COMMENT ON COLUMN func_log.created_time IS '생성 시점';
+COMMENT ON COLUMN func_log.created_at IS '생성 시점';
 
 
 -- func_log foreign keys
@@ -74,7 +74,7 @@ CREATE TABLE ml_project (
 	user_idx INTEGER NOT NULL, -- 사용자 고유 번호
 	proj_name VARCHAR(255) NOT NULL, -- 프로젝트 명
 	last_update TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 마지막 수정 시점
-	created_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 생성 시점
+	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 생성 시점
 	CONSTRAINT ml_project_pk PRIMARY KEY (idx)
 );
 COMMENT ON TABLE ml_project IS 'ML 프로젝트 정보';
@@ -85,7 +85,7 @@ COMMENT ON COLUMN ml_project.idx IS '프로젝트 고유 번호';
 COMMENT ON COLUMN ml_project.user_idx IS '사용자 고유 번호';
 COMMENT ON COLUMN ml_project.proj_name IS '프로젝트 명';
 COMMENT ON COLUMN ml_project.last_update IS '마지막 수정 시점';
-COMMENT ON COLUMN ml_project.created_time IS '생성 시점';
+COMMENT ON COLUMN ml_project.created_at IS '생성 시점';
 
 
 -- ml_project foreign keys
@@ -106,7 +106,7 @@ CREATE TABLE ml_model (
 	model_name VARCHAR(255) NOT NULL, -- 모델명
 	model_url VARCHAR(255) NOT NULL DEFAULT '', -- 모델 S3 URL
 	last_update TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 마지막 수정 시점
-	created_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 생성 시점
+	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 생성 시점
 	CONSTRAINT ml_model_pk PRIMARY KEY (idx)
 );
 COMMENT ON TABLE ml_model IS 'ML 모델 정보';
@@ -117,7 +117,7 @@ COMMENT ON COLUMN ml_model.idx IS '모델 고유 번호';
 COMMENT ON COLUMN ml_model.user_idx IS '사용자 고유 번호';
 COMMENT ON COLUMN ml_model.model_name IS '모델명';
 COMMENT ON COLUMN ml_model.last_update IS '마지막 수정 시점';
-COMMENT ON COLUMN ml_model.created_time IS '생성 시점';
+COMMENT ON COLUMN ml_model.created_at IS '생성 시점';
 
 
 -- ml_model foreign keys
@@ -125,29 +125,78 @@ COMMENT ON COLUMN ml_model.created_time IS '생성 시점';
 ALTER TABLE ml_model ADD CONSTRAINT ml_model_fk FOREIGN KEY (user_idx) REFERENCES users(idx);
 
 
+-- mail_confirm definition
 
--- users_inactive definition
+-- DROP TABLE mail_confirm;
 
--- DROP TABLE users_inactive;
+CREATE TABLE mail_confirm(
+	email VARCHAR(255) NOT NULL,
+	cert_number VARCHAR(255) NOT NULL,
+	last_update TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 마지막 인증 요청 시점
+	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 최초 인증 요청 시점
+);
 
-CREATE TABLE users_inactive(
-	idx SERIAL NOT NULL, -- 사용자 고유 번호
+COMMENT ON COLUMN mail_confirm.last_update IS '마지막 인증 요청 시점';
+COMMENT ON COLUMN mail_confirm.created_at IS '최초 인증 요청 시점';
+
+
+-- inactive_users definition
+
+-- DROP TABLE inactive_users;
+
+CREATE TABLE inactive_users(
+	idx INTEGER NOT NULL, -- 사용자 고유 번호
 	email VARCHAR(255) NOT NULL,
 	pw VARCHAR(255) NOT NULL, -- 패스워드
 	nickname VARCHAR(255) NOT NULL,
-	membership INTEGER NOT NULL DEFAULT 0, -- 멤버쉽 구분(티어1, 티어2 등)
-	last_update TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 마지막 접속 시점
-	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 가입 시점
-	user_state INTEGER NOT NULL, -- 사용자 상태(0: 휴면, 1: 탈퇴)
-	CONSTRAINT users_inactive_pk PRIMARY KEY (idx)
+	membership INTEGER NOT NULL, -- 멤버쉽 구분(티어1, 티어2 등)
+	last_update TIMESTAMP WITH TIME ZONE NOT NULL, -- 마지막 접속 시점
+	created_at TIMESTAMP WITH TIME ZONE NOT NULL, -- 가입 시점
+	user_state INTEGER NOT NULL DEFAULT 1, -- 사용자 상태(0: 휴면, 1: 탈퇴)
+	inactivated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- 사용자 상태(0: 휴면, 1: 탈퇴)
+	CONSTRAINT inactive_users_pk PRIMARY KEY (idx)
 );
-COMMENT ON TABLE users_inactive IS '비활성 사용자 정보';
+COMMENT ON TABLE inactive_users IS '비활성 사용자 정보';
 
 -- Column comments
 
-COMMENT ON COLUMN users_inactive.idx IS '사용자 고유 번호';
-COMMENT ON COLUMN users_inactive.pw IS '패스워드';
-COMMENT ON COLUMN users_inactive.membership IS '멤버쉽 구분(티어1, 티어2 등)';
-COMMENT ON COLUMN users_inactive.last_update IS '마지막 접속 시점';
-COMMENT ON COLUMN users_inactive.created_at IS '가입 시점';
-COMMENT ON COLUMN users_inactive.user_state IS '사용자 상태(0: 휴면, 1: 탈퇴)';
+COMMENT ON COLUMN inactive_users.idx IS '사용자 고유 번호';
+COMMENT ON COLUMN inactive_users.pw IS '패스워드';
+COMMENT ON COLUMN inactive_users.membership IS '멤버쉽 구분(티어1, 티어2 등)';
+COMMENT ON COLUMN inactive_users.last_update IS '마지막 접속 시점';
+COMMENT ON COLUMN inactive_users.created_at IS '가입 시점';
+COMMENT ON COLUMN inactive_users.user_state IS '사용자 상태(0: 휴면, 1: 탈퇴)';
+
+
+
+-- ★ Trigger 생성하기
+
+-- 5분 지난 인증 메일 발송 기록은 새로운 기록 들어올 때 자동 삭제시킴
+CREATE FUNCTION delete_old_mail_confirm() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $trigger_delete_old_mail_confirm$
+BEGIN
+  DELETE FROM mail_confirm WHERE timestamp < CURRENT_TIMESTAMP - INTERVAL '5 minutes';
+  RETURN NULL;
+END;
+$trigger_delete_old_mail_confirm$;
+
+CREATE TRIGGER trigger_delete_old_mail_confirm
+    AFTER INSERT ON mail_confirm
+    EXECUTE PROCEDURE delete_old_mail_confirm();
+
+
+
+-- 인증 요청이 중복해서 들어오면 해당 데이터의 last_update를 갱신시킴
+CREATE FUNCTION update_mail_confirm() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $trigger_update_mail_confirm$
+BEGIN
+  update mail_confirm set last_update=NOW() where email = new.email;
+  RETURN NULL;
+END;
+$trigger_update_mail_confirm$;
+
+CREATE TRIGGER trigger_update_mail_confirm
+    AFTER UPDATE ON mail_confirm for each row
+    EXECUTE PROCEDURE update_mail_confirm();
